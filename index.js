882a447a513972f02327981eb67691ca72ae001a
saung3754 <saung3754@nntuple.com>
2019-12-04 19:36:30 +0900

    req,res test

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const app = express();
 const PORT = 4000;
 
 const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Listening on: http://localhost:${PORT}`);
 };
+
+function handletest(req, res) {
+    console.log(req);
+    res.send("hellow world!");
+}
+
+function handleGo(req, res) {
+    res.send("this is handleGo");
+}
+
+app.get("/", handletest);
+
+app.get("/test", handleGo);
 
 app.listen(PORT, handleListening);
