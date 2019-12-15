6fa097ad0ff1b999621feb4a2b4fdac040c37921
saung3754 <saung3754@nntuple.com>
2019-12-15 23:29:01 +0900

    router 경로 재배치 pug 설치

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import userRouter from "./routers/useRouter";
+import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express();
+
+app.set("view engine","pug");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export const join =(req,res)=>res.send("Join");
+export const login =(req,res)=>res.send("login");
+export const logout =(req,res)=>res.send("logout");
+export const users =(req,res)=>res.send("Users");
+export const userDetail =(req,res)=>res.send("UserDetail");
+export const editProfile =(req,res)=>res.send("EditProfile");
+export const changePassword =(req,res)=>res.send("ChangePassword");
