ce0b836737af1eec4d70f210c24875a307b60162
권효주 <saung3754@nntuple.com>
2019-12-19 20:49:38 +0900

    join,login,home 화면 search 페이지 로직 추가

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Wetube
 
 Cloning Youtube with Vanilla and NodeJS
+
+## pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [X] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
+
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,22 @@
 export const home = (req, res) => res.render("home", { pageTitle: "home" });
-export const search = (req, res) =>
-    res.render("search", { pageTitle: "search" });
+
+export const search = (req, res) => {
+    const {
+        query: { terms: searchingBy }
+    } = req;
+    // const searchingBy = req.query.terms;
+
+    res.render("search", { pageTitle: "search", searchingBy });
+};
+
 export const upload = (req, res) =>
     res.render("upload", { pageTitle: "upload" });
+
 export const videoDetail = (req, res) =>
     res.render("videoDetail", { pageTitle: "home" });
+
 export const editVideo = (req, res) =>
     res.render("editVideo", { pageTitle: "editvideo" });
+
 export const deleteVideo = (req, res) =>
     res.render("deleteVideo", { pageTitle: "deleteVideo" });
