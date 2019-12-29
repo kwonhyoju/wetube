74f75ad03b51778222a65669e07d2cd4c7b696d0
saung3754 <saung3754@nntuple.com>
2019-12-29 22:51:42 +0900

    videoMixin 구성

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: "Video awesome",
         description: "This is sonething I love",
         view: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 12312,
             name: "hyoju",
@@ -16,7 +16,7 @@ export const videos = [
         title: "Video super",
         description: "This is sonething I love",
         view: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 12312,
             name: "hyoju",
@@ -28,7 +28,7 @@ export const videos = [
         title: "Video nice",
         description: "This is sonething I love",
         view: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 12312,
             name: "hyoju",
@@ -40,7 +40,7 @@ export const videos = [
         title: "Video happy",
         description: "This is sonething I love",
         view: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 12312,
             name: "hyoju",
