882b96e5438a364f789909e04a208c2b2bcdf069
saung3754 <saung3754@nntuple.com>
2020-01-02 23:02:56 +0900

    mongodb연결 dotenv설정

--- db.js
+++ db.js
@@ -1,50 +1,24 @@
-export const videos = [
-    {
-        id: 132133,
-        title: "Video awesome",
-        description: "This is sonething I love",
-        view: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12312,
-            name: "hyoju",
-            email: "hyo@ju.com"
-        }
-    },
-    {
-        id: 456789,
-        title: "Video super",
-        description: "This is sonething I love",
-        view: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12312,
-            name: "hyoju",
-            email: "hyo@ju.com"
-        }
-    },
-    {
-        id: 444444,
-        title: "Video nice",
-        description: "This is sonething I love",
-        view: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12312,
-            name: "hyoju",
-            email: "hyo@ju.com"
-        }
-    },
-    {
-        id: 447865,
-        title: "Video happy",
-        description: "This is sonething I love",
-        view: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12312,
-            name: "hyoju",
-            email: "hyo@ju.com"
-        }
-    }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+process.env.MONGO_URL,
+{
+    useNewUrlParser: true,
+    useFindAndModify: false
+}
+);
+
+const db= mongoose.connection;
+
+const handleOpen=()=>{
+    console.log("Connected to DB!!");
+}
+
+const handleError = (error)=>{
+    console.log(`Error on DB Connection ${error}`);
+}
+
+db.once("open",handleOpen);
+db.on("error",handleError);
