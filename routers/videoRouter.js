439f1b7e7c1e0fd0172e1285d9304d703c8743f7
saung3754 <saung3754@nntuple.com>
2020-01-13 22:46:57 +0900

    edit video page

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "../routes";
 import {
     videoDetail,
-    editVideo,
+    getEditVideo,
+    postEditVideo,
     deleteVideo,
     getUpload,
     postUpload
@@ -12,10 +13,18 @@ import { uploadVideo } from "../middlewares";
 const videoRouter = express.Router();
 
 // videoRouter.get(routes.videos,videos);
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
