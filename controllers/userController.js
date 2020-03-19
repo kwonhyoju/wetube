5ee6b5f70d6fad4f4f61f2da898ad3243befa6d7
권효주 <saung3754@nntuple.com>
2020-03-19 19:47:05 +0900

    setting passport , user 로그인 시 id값 쿠키에 저장 , user 로그인에 맞게 header변경

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,47 @@
+import passport from "passport";
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "join" });
-}
+};
 
-export const postJoin =(req,res) => {
+export const postJoin = async (req, res, next) => {
     // app.js에 있는 bodyparser로 유저가 작성한 값을 가져오는 console
     // console.log(req.body);
-    const {body:{name,email,password,password2}}=req;
+    const {
+        body: { name, email, password, password2 }
+    } = req;
 
-    if(password !== password2){
+    if (password !== password2) {
         res.status(400);
-        res.render("join",{pageTitle:"join"});
-    }else{
-        res.redirect(routes.home);
+        res.render("join", { pageTitle: "join" });
+    } else {
+        try {
+            const user = await User({
+                name,
+                email
+            });
+            await User.register(user, password);
+            next();
+        } catch (error) {
+            console.log(error);
+            res.redirect(routes.home);
+        }
     }
-}
+};
 
+export const getLogin = (req, res) =>
+    res.render("login", { pageTitle: "login" });
 
-export const getLogin = (req, res) => res.render("login", { pageTitle: "login" });
+export const postLogin = passport.authenticate("local", {
+    failureRedirect: routes.login,
+    successRedirect: routes.home
+});
 
-export const postLogin = (req,res)=>{
+export const logout = (req, res) => {
     res.redirect(routes.home);
-}
-
-
-export const logout = (req, res) =>{
-    res.redirect(routes.home);
-}
-
+};
 
 export const userDetail = (req, res) =>
     res.render("userDetail", { pageTitle: "userDetail" });
